c2dac9194a260584cca0dbce5083ff2084e23dd9
Aditya Katiyar <rishkat1249@gmail.com>
2026-06-20 17:15:17 +0530

    Adding embeddings

--- femto_gpt.c
+++ femto_gpt.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_VOCAB 32
 #define MAX_TOKENS 128
 #define WORD_LEN 32
+#define EMBED_DIM 4
 
 typedef struct {
-    char word[WORD_LEN];
+  char word[WORD_LEN];
 } Token;
 
 Token vocab[MAX_VOCAB];
@@ -17,268 +18,256 @@ int vocab_size = 0;
 int token_ids[MAX_TOKENS];
 int token_count = 0;
 
-int transition_counts[MAX_VOCAB][MAX_VOCAB]; 
-
+int transition_counts[MAX_VOCAB][MAX_VOCAB];
 float probability_table[MAX_VOCAB][MAX_VOCAB];
+float embeddings[MAX_VOCAB][EMBED_DIM];
 
 int get_token_id(const char *word) {
-    for (int i = 0; i < vocab_size; i++) {
-        if (strcmp(vocab[i].word, word) == 0) {
-            return i;
-        }
+  for (int i = 0; i < vocab_size; i++) {
+    if (strcmp(vocab[i].word, word) == 0) {
+      return i;
     }
+  }
 
-    strcpy(vocab[vocab_size].word, word);
-    vocab_size++;
-    return vocab_size - 1;
+  strcpy(vocab[vocab_size].word, word);
+  vocab_size++;
+  return vocab_size - 1;
 }
 
-void add_sentence(const char *sentence)
-{
-    token_ids[token_count++] = get_token_id("<BOS>");
+void add_sentence(const char *sentence) {
+  token_ids[token_count++] = get_token_id("<BOS>");
 
-    char buffer[256];
-    strcpy(buffer, sentence);
+  char buffer[256];
+  strcpy(buffer, sentence);
 
-    char *word = strtok(buffer, " ");
+  char *word = strtok(buffer, " ");
 
-    while (word != NULL)
-    {
-        token_ids[token_count++] = get_token_id(word);
-        word = strtok(NULL, " ");
-    }
+  while (word != NULL) {
+    token_ids[token_count++] = get_token_id(word);
+    word = strtok(NULL, " ");
+  }
 
-    token_ids[token_count++] = get_token_id("<EOS>");
+  token_ids[token_count++] = get_token_id("<EOS>");
 }
 
-void build_transition_counts()
-{
-    for (int i = 0; i < token_count - 1; i++)
-    {
-        int current = token_ids[i];
-        int next = token_ids[i + 1];
+void build_transition_counts() {
+  for (int i = 0; i < token_count - 1; i++) {
+    int current = token_ids[i];
+    int next = token_ids[i + 1];
 
-        transition_counts[current][next]++;
-    }
+    transition_counts[current][next]++;
+  }
 }
 
-void build_probability_table()
-{
-    for (int from = 0; from < vocab_size; from++)
-    {
-        int total_transitions = 0;
+void build_probability_table() {
+  for (int from = 0; from < vocab_size; from++) {
+    int total_transitions = 0;
 
-        for (int to = 0; to < vocab_size; to++)
-        {
-            total_transitions += transition_counts[from][to];
-        }
-
-        if (total_transitions == 0)
-        {
-            continue; 
-        }
-
-        for (int to = 0; to < vocab_size; to++)
-        {
-            probability_table[from][to] = 
-                (float)transition_counts[from][to] / total_transitions;
-        }
+    for (int to = 0; to < vocab_size; to++) {
+      total_transitions += transition_counts[from][to];
     }
+
+    if (total_transitions == 0) {
+      continue;
+    }
+
+    for (int to = 0; to < vocab_size; to++) {
+      probability_table[from][to] =
+          (float)transition_counts[from][to] / total_transitions;
+    }
+  }
 }
 
-int sample_next_token(int current_token)
-{
-    float r = (float)rand() / (float)RAND_MAX; 
-
-    float cumulative = 0.0f;
-
-    for (int next = 0; next < vocab_size; next++)
-    {
-        cumulative += probability_table[current_token][next];
-
-        if (r <= cumulative)
-        {
-            return next;
-        }
+void initialize_embeddings() {
+  for (int token = 0; token < vocab_size; token++) {
+    for (int dim = 0; dim < EMBED_DIM; dim++) {
+      embeddings[token][dim] = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
     }
+  }
+}
 
-    return -1; 
+int sample_next_token(int current_token) {
+  float r = (float)rand() / (float)RAND_MAX;
+
+  float cumulative = 0.0f;
+
+  for (int next = 0; next < vocab_size; next++) {
+    cumulative += probability_table[current_token][next];
+
+    if (r <= cumulative) {
+      return next;
+    }
+  }
+
+  return -1;
 }
 
 void print_vocab() {
-    printf("\nVOCAB TABLE:\n");
-    for (int i = 0; i < vocab_size; i++) {
-        printf("id %d -> %s\n", i, vocab[i].word);
-    }
+  printf("\nVOCAB TABLE:\n");
+  for (int i = 0; i < vocab_size; i++) {
+    printf("id %d -> %s\n", i, vocab[i].word);
+  }
 }
 
 void print_tokens() {
-    printf("\nTOKEN IDS:\n");
-    for (int i = 0; i < token_count; i++) {
-        printf("%d ", token_ids[i]);
-    }
-    printf("\n");
+  printf("\nTOKEN IDS:\n");
+  for (int i = 0; i < token_count; i++) {
+    printf("%d ", token_ids[i]);
+  }
+  printf("\n");
 
-    printf("\nTOKENS AS WORDS:\n");
-    for (int i = 0; i < token_count; i++) {
-        printf("%s ", vocab[token_ids[i]].word);
-    }
-    printf("\n");
+  printf("\nTOKENS AS WORDS:\n");
+  for (int i = 0; i < token_count; i++) {
+    printf("%s ", vocab[token_ids[i]].word);
+  }
+  printf("\n");
 }
 
-void print_transition_counts()
-{
-    printf("\nTRANSITION COUNTS\n\n");
+void print_transition_counts() {
+  printf("\nTRANSITION COUNTS\n\n");
 
-    for (int from = 0; from < vocab_size; from++)
+  for (int from = 0; from < vocab_size; from++) {
+    printf("%s:\n", vocab[from].word);
+
+    for (int to = 0; to < vocab_size; to++) {
+      int count = transition_counts[from][to];
+
+      if (count > 0) {
+        printf("   -> %-10s : %d\n", vocab[to].word, count);
+      }
+    }
+
+    printf("\n");
+  }
+}
+
+void print_probability_table() {
+  printf("\nPROBABILITY TABLE\n\n");
+
+  for (int from = 0; from < vocab_size; from++) {
+    printf("%s:\n", vocab[from].word);
+
+    for (int to = 0; to < vocab_size; to++) {
+      float probability = probability_table[from][to];
+
+      if (probability > 0.0f) {
+        printf("   -> %-10s : %.2f\n", vocab[to].word, probability);
+      }
+    }
+
+    printf("\n");
+  }
+}
+
+void print_embeddings() {
+
+    printf("\nEMBEDDINGS\n\n");
+
+    for(int token = 0; token < vocab_size; token++)
     {
-        printf("%s:\n", vocab[from].word);
+        printf("%-10s : ", vocab[token].word);
 
-        for (int to = 0; to < vocab_size; to++)
+        for (int dim = 0; dim < EMBED_DIM; dim++)
         {
-            int count = transition_counts[from][to];
-
-            if (count > 0)
-            {
-                printf(
-                    "   -> %-10s : %d\n",
-                    vocab[to].word,
-                    count
-                );
-            }
+            printf("%.3f ", embeddings[token][dim]);
         }
 
         printf("\n");
     }
 }
 
-void print_probability_table()
-{
-    printf("\nPROBABILITY TABLE\n\n");
+void debug_distribution(int token_id) {
+  printf("\nCurrent token: %s\n", vocab[token_id].word);
 
-    for (int from = 0; from < vocab_size; from++)
-    {
-        printf("%s:\n", vocab[from].word);
+  for (int i = 0; i < vocab_size; i++) {
+    float p = probability_table[token_id][i];
 
-        for (int to = 0; to < vocab_size; to++)
-        {
-            float probability = probability_table[from][to];
-
-            if (probability > 0.0f)
-            {
-                printf(
-                    "   -> %-10s : %.2f\n",
-                    vocab[to].word,
-                    probability
-                );
-            }
-        }
-
-        printf("\n");
+    if (p > 0.0f) {
+      printf("%s -> %.2f\n", vocab[i].word, p);
     }
+  }
 }
 
-void debug_distribution(int token_id)
-{
-    printf("\nCurrent token: %s\n", vocab[token_id].word);
 
-    for (int i = 0; i < vocab_size; i++)
-    {
-        float p = probability_table[token_id][i];
 
-        if (p > 0.0f)
-        {
-            printf(
-                "%s -> %.2f\n",
-                vocab[i].word,
-                p
-            );
-        }
+void generate_text(const char *start_word, int max_steps) {
+  int current = get_token_id(start_word);
+
+  printf("\nGenerated:\n");
+  printf("%s ", vocab[current].word);
+
+  for (int step = 0; step < max_steps; step++) {
+    debug_distribution(current);
+
+    int next = sample_next_token(current);
+
+    if (next == -1) {
+      break;
     }
+
+    if (strcmp(vocab[next].word, "<EOS>") == 0) {
+      break;
+    }
+
+    printf("%s ", vocab[next].word);
+
+    current = next;
+  }
+
+  printf("\n");
 }
 
-void generate_text(const char *start_word, int max_steps)
-{
-    int current = get_token_id(start_word);
+void generate_from_bos(int max_steps) {
+  int current = get_token_id("<BOS>");
 
-    printf("\nGenerated:\n");
-    printf("%s ", vocab[current].word);
+  printf("\nGenerated from <BOS>:\n");
 
-    for (int step = 0; step < max_steps; step++)
-    {
-        debug_distribution(current);
+  for (int step = 0; step < max_steps; step++) {
+    debug_distribution(current);
 
-        int next = sample_next_token(current);
+    int next = sample_next_token(current);
 
-        if (next == -1)
-        {
-            break;
-        }
-
-        if (strcmp(vocab[next].word, "<EOS>") == 0)
-        {
-            break;
-        }
-
-        printf("%s ", vocab[next].word);
-
-        current = next;
+    if (next == -1) {
+      break;
     }
 
-    printf("\n");
-}
-
-void generate_from_bos(int max_steps)
-{
-    int current = get_token_id("<BOS>");
-
-    printf("\nGenerated from <BOS>:\n");
-
-    for (int step = 0; step < max_steps; step++)
-    {
-        debug_distribution(current);
-
-        int next = sample_next_token(current);
-
-        if (next == -1)
-        {
-            break;
-        }
-
-        if (strcmp(vocab[next].word, "<EOS>") == 0)
-        {
-            break;
-        }
-
-        printf("%s ", vocab[next].word);
-
-        current = next;
+    if (strcmp(vocab[next].word, "<EOS>") == 0) {
+      break;
     }
 
-    printf("\n");
+    printf("%s ", vocab[next].word);
+
+    current = next;
+  }
+
+  printf("\n");
 }
 
 int main() {
-    srand(time(NULL)); 
-    add_sentence("cat eats fish");
-    add_sentence("dog eats food");
-    add_sentence("cat drinks milk");
-    add_sentence("dog drinks water");
+  srand(time(NULL));
+  add_sentence("cat eats fish");
+  add_sentence("dog eats food");
+  add_sentence("cat drinks milk");
+  add_sentence("dog drinks water");
 
-    print_vocab();
-    print_tokens();
+  print_vocab();
+  print_tokens();
 
-    build_transition_counts();
+  build_transition_counts();
 
-    print_transition_counts();
+  print_transition_counts();
 
-    build_probability_table();
+  build_probability_table();
 
-    print_probability_table();
+  print_probability_table();
 
-    generate_text("cat", 10);
-    generate_text("dog", 10);
-    generate_from_bos(10);
-    
-    return 0;
+  initialize_embeddings();
+
+  print_embeddings();
+
+  generate_text("cat", 10);
+  generate_text("dog", 10);
+  generate_from_bos(10);
+
+  return 0;
 }
